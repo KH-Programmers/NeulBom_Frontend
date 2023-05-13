706f23f919480b10644096b7540e6aebb216db17
ZerOne <zerone@ZerOnes-MacBook.local>
2023-05-13 13:57:32 +0900

    feat: welcome page

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+export default function Index() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#FAE8FF] via-[#E2E8F0] to-[#FFFCED]">
+      <div className="flex items-center h-screen">
+        <div className="px-40">
+          <h1 className="font-black text-5xl text-left">
+            <p>Always Look at</p>
+            <p>My own school,</p>
+          </h1>
+          <h1 className="font-black text-6xl text-left">늘봄</h1>
+          <div className="mt-6 bg-[#30304B] w-min text-xl rounded-2xl drop-shadow-md">
+            <h1 className="whitespace-nowrap text-white font-black px-20 py-4">
+              바로가기
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
