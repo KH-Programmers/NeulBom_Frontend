0a99b07941fbbb506da4f9979b50334471d320af
ZerOne <zerone@ZerOnes-MacBook.local>
2023-05-13 17:08:17 +0900

    refactor: update gradient color, responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 
 import Index from "./pages";
+import Login from "./pages/login";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Index />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,13 @@ export default function Index() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#FAE8FF] via-[#E2E8F0] to-[#FFFCED]">
       <div className="flex items-center h-screen">
-        <div className="px-40">
-          <h1 className="font-black text-5xl text-left">
-            <p>Always Look at</p>
-            <p>My own school,</p>
+        <div className="px-10 md:px-40">
+          <h1 className="font-black text-4xl md:text-5xl text-left md:w-1/2 bg-gradient-to-br text-transparent bg-clip-text from-[#008171] to-[#0070C1]" style={{ lineHeight: "1.1" }}>
+            Always Look at My own school,
           </h1>
-          <h1 className="font-black text-6xl text-left">늘봄</h1>
+          <h1 className="mt-1 font-black text-5xl md:text-6xl text-left bg-gradient-to-br text-transparent bg-clip-text from-[#E074A4] to-[#BE123C]">늘봄</h1>
           <div className="mt-6 bg-[#30304B] w-min text-xl rounded-2xl drop-shadow-md">
-            <h1 className="whitespace-nowrap text-white font-black px-20 py-4">
+            <h1 className="whitespace-nowrap text-white font-black px-14 md:px-20 py-3 md:py-4">
               바로가기
             </h1>
           </div>
