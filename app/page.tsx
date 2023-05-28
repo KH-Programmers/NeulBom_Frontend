ad876bcf8c2d23fdcf3e225e165dd9fe3e7d34e6
Song Chanwoo <me@zero0ne.dev>
2023-05-28 17:54:57 +0900

    fix: redirect structure

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,15 @@ import { useEffect, useState } from "react";
 const Page = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      if (localStorage.getItem("visited") == null) {
-        localStorage.setItem("visited", "true");
+      const localData = [
+        localStorage.getItem("accessToken"),
+        localStorage.getItem("refreshToken"),
+      ];
+      const sessionData = [
+        sessionStorage.getItem("accessToken"),
+        sessionStorage.getItem("refreshToken"),
+      ];
+      if (!localData[0] && !localData[1] && !sessionData[0] && !sessionData[1]) {
         window.location.href = '/welcome';
       }
     }
