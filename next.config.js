a4881d244522c82f600245ea8c8c2cff687d0d3e
paring <paring@pikokr.dev>
2023-05-31 23:30:25 +0900

    fix: completely disable next-pwa on dev environmnet

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  // enable pwa only in production environment
-  disable: process.env.NODE_ENV !== "production",
-});
-
-module.exports = withPWA({
+const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
-});
+};
+
+if (process.env.NODE_ENV === "production") {
+  const withPWA = require("next-pwa")({
+    dest: "public",
+    // enable pwa only in production environment
+    disable: process.env.NODE_ENV !== "production",
+  });
+
+  module.exports = withPWA(nextConfig);
+} else {
+  module.exports = nextConfig;
+}
