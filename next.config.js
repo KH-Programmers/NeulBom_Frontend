378298d6b1345d40ddc96fb32449c22392f9ac7b
paring <paring@pikokr.dev>
2023-05-31 23:23:22 +0900

    fix: GenerateSW has been called multiple times

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {};
 
 const withPWA = require("next-pwa")({
   dest: "public",
+  // enable pwa only in production environment
+  disable: process.env.NODE_ENV !== "production",
 });
 
 module.exports = withPWA({
