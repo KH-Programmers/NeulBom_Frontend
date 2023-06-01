dc0ae8868af70009b2145d0ea1b4669806a2fda6
paring <paring@pikokr.dev>
2023-06-01 23:02:22 +0900

    fix: rollback comment input

--- app/app/posts/[id]/components/CommentInput.tsx
+++ app/app/posts/[id]/components/CommentInput.tsx
@@ -3,31 +3,35 @@
 import clsx from "clsx";
 import React from "react";
 import { TbSend } from "react-icons/tb";
-import MDEditor from "@uiw/react-md-editor";
-import rehypeSanitize from "rehype-sanitize";
 
 export const CommentInput: React.FC<{ nested?: boolean }> = ({ nested }) => {
   const [value, setValue] = React.useState("");
 
   return (
     <div>
-      <MDEditor
-        value={value}
-        onChange={(v) => setValue(v ?? "")}
-        previewOptions={{
-          rehypePlugins: [rehypeSanitize],
-        }}
-        preview="edit"
-      />
+      <textarea
+        rows={4}
+        className={clsx(
+          "bg-black/5 w-full resize-none rounded-xl focus:outline-none p-4",
+          {
+            "text-sm": nested,
+          }
+        )}
+      ></textarea>
       <div className="flex mt-2">
         <div className="flex-grow"></div>
         <button
           onClick={() => {
             alert(value);
           }}
-          className="bg-blue-500 text-white px-4 py-2 hover:brightness-90 active:brightness-75 transition-all rounded-lg flex gap-2 items-center"
+          className={clsx(
+            "bg-blue-500 text-white px-4 py-2 hover:brightness-90 active:brightness-75 transition-all rounded-lg flex gap-2 items-center",
+            {
+              "text-sm": nested,
+            }
+          )}
         >
-          <TbSend />
+          <TbSend size={nested ? 14 : 16} />
           등록하기
         </button>
       </div>
