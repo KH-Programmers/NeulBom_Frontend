5eb6d05a296d41c874cb9aea1ac3cffb8fc95a2a
paring <paring@pikokr.dev>
2023-06-01 23:06:21 +0900

    fix: navigator is not defined

--- app/app/posts/[id]/page.tsx
+++ app/app/posts/[id]/page.tsx
@@ -86,7 +86,7 @@ const PostViewPage: React.FC = () => {
             <div className="flex-grow w-0" />
             <button
               className="border-2 p-2 border-blue-500 text-blue-500 rounded-lg flex gap-2 items-center hover:bg-blue-500 hover:text-white transition-all disabled:border-black/20 disabled:text-black/20 disabled:hover:border-black/20 disabled:hover:text-black/20 disabled:hover:bg-transparent"
-              disabled={!navigator.share}
+              disabled={typeof navigator === "undefined" || !navigator.share}
               onClick={() => {
                 navigator.share({
                   title: document.title,
