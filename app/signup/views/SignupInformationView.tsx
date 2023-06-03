d9b1a2c3ceec662949a199de97c3032a94213d8d
ZeroOneDeveloper <me@zero0ne.dev>
2023-06-03 15:59:31 +0900

    feat: sign up function

--- app/signup/views/SignupInformationView.tsx
+++ app/signup/views/SignupInformationView.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { FormProvider, useForm } from "react-hook-form";
+
+import axios, { AxiosError } from "axios";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Turnstile } from "@marsidev/react-turnstile";
+
 import { FormLabel } from "@/components/FormLabel";
 import { FormInput } from "@/components/FormInput";
 import { Button } from "@/components/Button";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
 
 const schema = yup
   .object({
@@ -25,13 +29,36 @@ export const SignupInformationView: React.FC<{ next: () => void }> = ({
 }) => {
   const form = useForm<FormData>({ resolver: yupResolver(schema) });
   const { register, handleSubmit } = form;
+  const [token, setToken] = useState("");
 
   return (
     <FormProvider {...form}>
       <form
         className="mt-4 w-full max-w-[420px] mx-auto flex flex-col gap-4"
-        onSubmit={handleSubmit((data) => {
-          next();
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            const response = await axios.post(
+              `${process.env.NEXT_PUBLIC_API_URI!}/user/register/`,
+              {
+                username: data.name,
+                grade: data.studentId,
+                email: data.email,
+                password: data.password,
+                token: token,
+              }
+            );
+            if (response.status === 201) {
+              next();
+            }
+          } catch (e) {
+            const error = e as AxiosError;
+            switch (error.response?.status) {
+              case 400:
+                return alert("잘못된 정보를 입력했습니다. 다시 확인해주세요.");
+              case 406:
+                return alert("캡챠 인증에 실패했습니다. 다시 시도해주세요.");
+            }
+          }
         })}
       >
         <FormLabel
@@ -78,6 +105,13 @@ export const SignupInformationView: React.FC<{ next: () => void }> = ({
         >
           비밀번호 재입력
         </FormLabel>
+        <Turnstile
+          siteKey={process.env.NEXT_PUBLIC_TURNSTLIE_SITE_KEY!}
+          className="mt-4 mx-auto"
+          onSuccess={(token) => {
+            setToken(token);
+          }}
+        />
         <Button type="submit" className="mt-4">
           가입하기
         </Button>
