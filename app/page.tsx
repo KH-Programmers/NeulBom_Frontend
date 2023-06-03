8ef23535573815e248f9a12a89343ea58f74c769
ZeroOneDeveloper <me@zero0ne.dev>
2023-06-03 17:04:26 +0900

    refactor(fix): localStorage, sessionStroage value

--- app/page.tsx
+++ app/page.tsx
@@ -5,20 +5,9 @@ import { useEffect, useState } from "react";
 const Page = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
-      const localData = [
-        localStorage.getItem("accessToken"),
-        localStorage.getItem("refreshToken"),
-      ];
-      const sessionData = [
-        sessionStorage.getItem("accessToken"),
-        sessionStorage.getItem("refreshToken"),
-      ];
-      if (
-        !localData[0] &&
-        !localData[1] &&
-        !sessionData[0] &&
-        !sessionData[1]
-      ) {
+      const localData = localStorage.getItem("token");
+      const sessionData = sessionStorage.getItem("token");
+      if (!localData && !sessionData) {
         window.location.href = "/welcome";
       }
     }
