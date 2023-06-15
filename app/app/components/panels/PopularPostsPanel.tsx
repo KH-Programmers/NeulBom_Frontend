131bee1b838b7c306cad0da461dcb2d3b05317c4
현중 <yeonhj0507@naver.com>
2023-06-15 21:45:55 +0900

    fix:todayMeal - yeonhj0507

--- app/app/components/panels/PopularPostsPanel.tsx
+++ app/app/components/panels/PopularPostsPanel.tsx
@@ -18,7 +18,7 @@ export const PopularPostsPanel: React.FC<{
           <div key={k} className="p-4">
             <div className="text-xl">{x.title}</div>
             <div className="mt-1 font-light text-sm overflow-hidden text-ellipsis text-black/60">
-              {x.authorName}
+              익명
             </div>
           </div>
         );
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { cookies } from "next/headers";
-
 import { GET } from "@/utils/request";
 import { MainCard } from "./components/MainCard";
 import { MainBanner } from "./components/MainBanner";
@@ -10,8 +8,6 @@ import { TodayTodoPanel } from "./components/panels/TodayTodoPanel";
 import { PopularPostsPanel } from "./components/panels/PopularPostsPanel";
 
 export default async function AppMain() {
-  const cookieStore = cookies();
-  const token = cookieStore.get("token");
   let data: {
     foodData?: {
       date: number;
@@ -36,36 +32,6 @@ export default async function AppMain() {
       username: string;
     };
   } = {
-    foodData: {
-      date: 20230602,
-      lunchData: [
-        ["계란야채볶음밥/치즈함박", [1, 2, 5, 6, 10, 12, 13, 15, 16]],
-        ["미소국", [5, 6]],
-        ["볼어묵조림", [1, 5, 6, 13]],
-        ["배추김치", [9]],
-        ["카프리썬", [11, 12]],
-        ["꽃맛살샐러드", [1, 5, 6, 8, 13, 18]],
-      ],
-      dinnerData: [
-        ["참치주먹밥", [1, 5]],
-        ["돈코츠라멘", [1, 2, 5, 6, 10, 13, 15, 16]],
-        ["배추김치", [9]],
-        ["팝콘", [2, 5]],
-        ["초코우유", [2]],
-        ["열대과일샐러드", [1, 2, 5, 6, 11]],
-      ],
-    },
-    articleData: [
-      {
-        id: 5,
-        authorName: "flandrescarlet",
-        title: "한국사 너무 어려워요",
-        commentCount: 0,
-        viewCounts: 445,
-        updatedAt: "2023-06-09T15:34:52.326915",
-        likeCount: 18,
-      },
-    ],
     eventData: [
       {
         date: 20230604,
