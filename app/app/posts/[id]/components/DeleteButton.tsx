b50290cc0c4955ceb5ab2f38dd2619e7d3f7c113
Song Chanwoo <me@zero0ne.dev>
2023-07-15 19:51:45 +0900

    feat: fix redirect before delete post

--- app/app/posts/[id]/components/DeleteButton.tsx
+++ app/app/posts/[id]/components/DeleteButton.tsx
@@ -2,20 +2,17 @@
 
 import { DELETE } from "@/utils/request";
 import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
-import { useRouter } from "next/navigation";
 import { AxiosError } from "axios";
 
 export const DeleteButton: React.FC<{ id: string; token: RequestCookie }> = ({
   id,
   token,
 }) => {
-  const { push } = useRouter();
-
   const deletePost = async () => {
     try {
       const response = await DELETE(`/board/study/${id}/`, token.value); ///url은 후에 category 받아와서 수정.
       if (response.status === 204) {
-        push("/app/board/all");
+        window.location.replace("/app/board/all");
       }
     } catch (e) {
       const error = e as AxiosError;
