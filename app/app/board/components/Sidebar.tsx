10234a2c745d0fa1594ffa83398de4c35a562f20
현중 <yeonhj0507@naver.com>
2023-07-17 13:40:50 +0900

    chore:write btn appears at popular category

--- app/app/board/components/Sidebar.tsx
+++ app/app/board/components/Sidebar.tsx
@@ -10,12 +10,13 @@ export const BoardListSidebar: React.FC<{
   categories: BoardCategory[];
 }> = ({ categories }) => {
   const selectedCategory = useSelectedCategory();
+  const isPopular = (selectedCategory == "popular");
   return (
     <div className="static md:sticky top-[88px] self-start bg-white p-4 w-full md:w-fit min-w-[260px] rounded-xl shadow-md">
       {categories.map((x, i) => (
         <SidebarItem key={i} category={x} />
       ))}
-      <Link
+      {!isPopular && (<Link
         href={{
           pathname: "/app/posts",
           query: { category: selectedCategory },
@@ -24,7 +25,7 @@ export const BoardListSidebar: React.FC<{
         className="text-center mt-10 block transition-colors p-1 rounded-lg bg-primary text-white"
       >
         글쓰기
-      </Link>
+      </Link>)}
     </div>
   );
 };
