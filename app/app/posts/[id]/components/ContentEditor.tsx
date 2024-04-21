796016b3ca9ce7b8599c76dc3420aa993f80ba46
ZeroOneDeveloper <me@zero0ne.dev>
2024-04-21 14:04:35 +0900

    fix: build error

--- app/app/posts/[id]/components/ContentEditor.tsx
+++ app/app/posts/[id]/components/ContentEditor.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import React from "react";
-import Select, { ActionMeta, SingleValue } from "react-select";
-import MDEditor from "@uiw/react-md-editor/esm";
-import { POST } from "@/utils/request";
-import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { useRouter } from "next/navigation";
+import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
+
 import { AxiosError } from "axios";
+import MDEditor from '@uiw/react-md-editor';
+import Select, { ActionMeta, SingleValue } from "react-select";
+
+import { POST } from "@/utils/request";
 
 const categories = [
   {
--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    esmExternals: "loose",
+  },
 };
 
 module.exports = nextConfig;
